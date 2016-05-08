5e1c80862a3c074aab5ca9858e2432102ae5cad8
miu <miu@miu-no-MacBook.local>
2016-05-08 15:41:34 +0900

    changed

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,26 @@
 var Vue = require('vue');
+Vue.use(require('vue-resource'));   // xhr用
 
-var app = new Vue({
-  el: '#app',
-  data: {
-    // ...
-  },
-  methods: {
-    // ...
-  },
+/**
+ * ルートオブジェクト??
+ */
+var vm = new Vue({
+    el: 'body',
+    data: {
+        hoge: 'hogehoge',
+        showPage: 0 // 0: 一覧、1: 承認画面
+    },
+    computed: {
+        isList: function () {
+            return this.showPage === 0;
+        },
+        isApprove: function () {
+            return this.showPage === 1;
+        }
+    },
+    methods: {
+        test: function () {
+            this.showPage = this.showPage === 0 ? 1 : 0;
+        }
+    }
 });
-
-console.log('hello world!');
-console.log('hello world2!');
